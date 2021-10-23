a860e22380fd4ff82e485b5cf66e468b15606ce7
冯靖 <sxyx2008@163.com>
2021-10-23 23:35:21 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ file="images"
 
 while read -r line
 do
-	new_image=`echo ${line#*/} | sed 's|/|-|g'`
+	new_image=`echo ${line##*/}`
 	echo "docker pull ${line}"
 	echo "docker tag ${line} v5cn/${new_image}"
 	echo "docker push v5cn/${new_image}"
